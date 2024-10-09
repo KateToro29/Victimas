b4f4b83e797635bcaa1771eb0c9b5a0b40f98bd6
Katherine Toro <katerine29echeverry@gmail.com>
2024-10-09 11:00:34 -0500

    se agregan estilos y se realiza configuracion de tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-
 import SearchApartments from  './components/SearchApartments.vue'
 import Button from 'primevue/button';
 
 </script>
 
 <template>
-  <Button label="Click me" icon="pi pi-check" />
+  
   <SearchApartments />
 </template>
--- src/components/SearchApartments.vue
+++ src/components/SearchApartments.vue
@@ -1,10 +1,10 @@
 
 <template>
-    <div class="card flex justify-center">
+    <div class="card flex  justify-center">
         <Select v-model="selectedCountry" :options="departamentos" optionLabel="name" placeholder="Seleccione el Departamento" class="w-full md:w-56">
             <template #value="slotProps">
-                <div v-if="slotProps.value" class="flex items-center">
-                    <img :alt="slotProps.value.label" :src="slotProps.flag" :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+                <div v-if="slotProps.value" class="flex gap-2 items-center">
+                    <img :alt="slotProps.value.name" :src="slotProps.value.flagUrl" :class="` flag flag-${slotProps.value.code.toLowerCase()}`" style="inline-size: 20px" />
                     <div>{{ slotProps.value.name }}</div>
                 </div>
                 <span v-else>
@@ -12,70 +12,63 @@
                 </span>
             </template>
             <template #option="slotProps">
-                <div class="flex items-center">
-                    <img :alt="slotProps.option.label" :src="slotProps.flag" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="inline-size: 18px" />
-                    <div>{{ slotProps.option.name }}</div>
+                <div class="flex  items-center">
+                    <img :alt="slotProps.name" :src="slotProps.option.flagUrl" :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="inline-size: 20px" />
+                    <p>{{ slotProps.option.name }}</p>
                 </div>
             </template>
             <template #dropdownicon>
                 <i class="pi pi-map"></i>
             </template>
-            <template #header>
-                <div class="font-medium p-3">Available departamentos</div>
-            </template>
-            <template #footer>
-                <div class="p-3">
-                    <Button label="Add New" fluid severity="secondary" text size="small" icon="pi pi-plus"></Button>
-                </div>
-            </template>
+           
+            
         </Select>
         
-        <img :src="amazonasFlag" alt="">
+        
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import Select from "primevue/select";
-import Button from "primevue/button";
-import amazonasFlag from '../assets/webp/amazonas.webp';
+
+
 
 
 const selectedCountry = ref();
 const departamentos = ref([
-    { name: 'Amazonas', code: '91', flagUrl: 'amazonasFlag'},
-    { name: 'Antioquia', code: '05', flag:'' },
-    { name: 'Arauca', code: '81', flag:'' },
-    { name: 'Atlantico', code: '08', flag:'' },
-    { name: 'Bogota D.C', code: '11' },
-    { name: 'Bolivar', code: '13' },
-    { name: 'Boyaca', code: '15' },
-    { name: 'Caldas', code: '17' },
-    { name: 'Caqueta ', code: '18' },
-    { name: 'Casanare', code: '85' },
-    { name: 'Cauca', code: '19' },
-    { name: 'Cesar', code: '20' },
-    { name: 'Choco', code: '27' },
-    { name: 'Cundinamarca ', code: '25' },
-    { name: 'Cordoba ', code: '23' },
-    { name: 'Guainia ', code: '94' },
-    { name: 'Guaviare ', code: '95' },
-    { name: 'Huila ', code: '41' },
-    { name: 'La Guajira ', code: '44' },
-    { name: 'Magdalena ', code: '47' },
-    { name: 'Meta ', code: '50' },
-    { name: 'Nariño ', code: '52' },
-    { name: 'Norte de Santander ', code: '54' },
-    { name: 'Putumayo ', code: '86' },
-    { name: 'Quindio ', code: '63' },
-    { name: 'Risaralda ', code: '66' },
-    { name: 'Archipièlago de San Andres,Providencia y Santa catalina ', code: '88' },
-    { name: 'Santander ', code: '68' },
-    { name: 'Sucre ', code: '70' },
-    { name: 'Tolima ', code: '73' },
-    { name: 'Valle del Cauca ', code: '76' },
-    { name: 'Vaupes ', code: '97' },
-    { name: 'Vichada ', code: '99' },
+    { name: 'Amazonas', code: '91', flagUrl: 'src/assets/webp/amazonas.webp'},
+    { name: 'Antioquia', code: '05', flagUrl:'src/assets/webp/antioquia.webp' },
+    { name: 'Arauca', code: '81', flagUrl:'src/assets/webp/arauca-departamento.webp' },
+    { name: 'Atlantico', code: '08', flagUrl:'src/assets/webp/atlantico.webp' },
+    { name: 'Bolivar', code: '13', flagUrl:'src/assets/webp/bolivar.webp' },
+    { name: 'Boyaca', code: '15', flagUrl:'src/assets/webp/boyaca.webp' },
+    { name: 'Caldas', code: '17', flagUrl:'src/assets/webp/caldas.webp' },
+    { name: 'Caqueta ', code: '18', flagUrl:'src/assets/webp/caqueta.webp' },
+    { name: 'Casanare', code: '85', flagUrl:'src/assets/webp/casanare.webp' },
+    { name: 'Cauca', code: '19', flagUrl:'src/assets/webp/cauca.webp' },
+    { name: 'Cesar', code: '20', flagUrl:'src/assets/webp/cesar.webp' },
+    { name: 'Choco', code: '27', flagUrl:'src/assets/webp/choco.webp' },
+    { name: 'Cundinamarca ', code: '25', flagUrl:'src/assets/webp/cundinamarca.webp' },
+    { name: 'Cordoba ', code: '23', flagUrl:'src/assets/webp/cordoba.webp' },
+    { name: 'Guainia ', code: '94', flagUrl:'src/assets/webp/guainia.webp' },
+    { name: 'Guaviare ', code: '95', flagUrl:'src/assets/webp/guaviare.webp' },
+    { name: 'Huila ', code: '41', flagUrl:'src/assets/webp/huila.webp' },
+    { name: 'La Guajira ', code: '44', flagUrl:'src/assets/webp/guajira.webp' },
+    { name: 'Magdalena ', code: '47', flagUrl:'src/assets/webp/magdalena.webp' },
+    { name: 'Meta ', code: '50', flagUrl:'src/assets/webp/meta.webp' },
+    { name: 'Nariño ', code: '52', flagUrl:'src/assets/webp/nariño.webp' },
+    { name: 'Norte de Santander ', code: '54', flagUrl:'src/assets/webp/norte-de-santander.webp' },
+    { name: 'Putumayo ', code: '86', flagUrl:'src/assets/webp/putumayo.webp' },
+    { name: 'Quindio ', code: '63', flagUrl:'src/assets/webp/quindio.webp' },
+    { name: 'Risaralda ', code: '66', flagUrl:'src/assets/webp/risaralda.webp' },
+    { name: 'Archipièlago de San Andres,Providencia y Santa catalina ', code: '88', flagUrl:'src/assets/webp/san-andrc3a9s-y-providencia.webp' },
+    { name: 'Santander ', code: '68', flagUrl:'src/assets/webp/santander.webp' },
+    { name: 'Sucre ', code: '70', flagUrl:'src/assets/webp/sucre.webp' },
+    { name: 'Tolima ', code: '73', flagUrl:'src/assets/webp/tolima.webp' },
+    { name: 'Valle del Cauca ', code: '76', flagUrl:'src/assets/webp/valle-del-cauca.webp' },
+    { name: 'Vaupes ', code: '97', flagUrl:'src/assets/webp/vaupes.webp' },
+    { name: 'Vichada ', code: '99', flagUrl:'src/assets/webp/vichada.webp' },
     
 
 
